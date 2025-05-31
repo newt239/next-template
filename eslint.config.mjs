15138d3ab1578f066aeb919e0c480c3d62e609c7
newt <ysti2019.win2020.st@gmail.com>
2025-06-01 00:05:18 +0900

    chore: setup Anthony's ESLint config preset

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,5 @@
-import css from "@eslint/css";
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import antfu from '@antfu/eslint-config'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    files: ["**/*.css"],
-    language: "css/css",
-    plugins: { css },
-    extends: ["css/recommended"],
-  },
-];
-
-export default eslintConfig;
+export default antfu({
+  react: true,
+})
