3684072edfac1749c0d592d6f8afdf3008fb894f
newt <ysti2019.win2020.st@gmail.com>
2026-01-31 20:56:36 +0900

    fix: type error

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -2,7 +2,7 @@
 
 import { Toaster as ToasterPrimitive, type ToasterProps } from "sonner";
 import { twJoin } from "tailwind-merge";
-import { useTheme } from "#/components/theme-provider";
+import { useTheme } from "#/components/providers/theme-provider";
 
 export function Toast(props: ToasterProps) {
   const { theme = "system" } = useTheme();
