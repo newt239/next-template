e30a7d894c51806c95eff96f0fad1a22a7f7d719
newt <ysti2019.win2020.st@gmail.com>
2026-08-22 21:28:12 +0900

    fix: エラーログにリクエストヘッダを出力しないようにする

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,9 @@
 import type { Instrumentation } from "next";
 
 export const onRequestError: Instrumentation.onRequestError = (error, request, context) => {
-  console.error("[onRequestError]", { context, error, request });
+  console.error("[onRequestError]", {
+    context,
+    error,
+    request: { method: request.method, path: request.path },
+  });
 };
